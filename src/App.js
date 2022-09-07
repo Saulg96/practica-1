26a9529a0e5259a416da2bdc1c391d5fd8488ec8
Saulg96 <saulg1081@gmail.com>
2022-09-07 07:59:58 -0600

    upload project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import User from './components/User';
 
 function App() {
+  const itemList = [
+    {number: 526, name: "Carlos", clasified: true, container: "container-1"},
+    {number: 866, name: "Alex", clasified: true, container: "container-2"},
+    {number: 940, name: "Sabrina", clasified: false, container: "container-3"},
+    {number: 955, name: "Carmen", clasified: false, container: "container-4"},
+    {number: 1053, name: "Esteban", clasified: true, container: "container-5"}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
+      
+      {itemList.map((item,id) => {
+        return (
+          <User key={id} number={item.number} name={item.name} clasify={item.clasified} />
+        )
+      })}
     </div>
   );
 }
